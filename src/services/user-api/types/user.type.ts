a6544ebbf0e8f20da6b1454042ff96d6f3b6fbf3
Yohannes Teshome <katteyohannes@gmail.com>
2024-08-01 08:54:37 -0400

    update prisma schema and add more user type

--- src/services/user-api/types/user.type.ts
+++ src/services/user-api/types/user.type.ts
@@ -1,29 +1,60 @@
 
 const usertype = `
     scalar DataTime
-    type Fullname{
-        firstname: string
-        middlename: string
-        lastname: string
+    type User {
+        id: ID
+        username: String
+        password: String
+        profile: Profile
+        isActive: Boolean
+        createdAt: DateTime
+        updatedAt: DateTime
+    }
+
+    type UserConnection{
+        totalCount: Int
+        node: [User]
+    }
+
+    type Address{
+        type String
+        state String?
+        city String?
+        country String?
+        point Point?
+    }
+
+    type Point{
+        latitude Float
+        longtude Float
     }
 
     type Profile{
-        fullname: Fullname
+        firstname: String
+        lastname: String
+        profilePictureUrl: String
         gender: Gender
         bod: DateTime
-    }
-    type User {
-        id: string
-
-        profile: Profile
+        address: Address
     }
 
     type Query {
-    
+        user(id: ID!): User
+        userList: UserConnection!
+    }
+
+    input RegisterUserInput{
+        username: String!
+        password: String!
     }
 
     type Mutation {
-    
+        registerUser(input: RegisterUserInput): User
+        createProfile(input: ProfileInput): Profile
+        updateProfile(input: ProfileUpdateInput): Profile
+        
+        deleteUser(id: ID!): User
+
     }
 
     enum Gender{
