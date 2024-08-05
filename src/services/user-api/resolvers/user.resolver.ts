65f113956ed53da46d3dd205619344d5c35c5669
Yohannes Teshome <katteyohannes@gmail.com>
2024-08-05 14:25:11 -0400

    add registerUser mutation

--- src/services/user-api/resolvers/user.resolver.ts
+++ src/services/user-api/resolvers/user.resolver.ts
@@ -2,14 +2,37 @@ import type { GraphQLContext } from "@config/context"
 
 const userResolvers = {
     Query: {
-
+        user: async (parent: unknown, args: { id: number }, context: GraphQLContext) => {
+            try{
+                const data = await context.prisma.user.findFirstOrThrow({
+                    where: {
+                        id: args?.id
+                    }
+                })
+                return data
+            }catch(err){
+                throw new Error("error occured white user")
+            }
+        },
+        userList: async (parent: unknown, args: {}, context: GraphQLContext) => {
+            try{
+                const data = await context.prisma.user.findRaw()
+                return data
+            }catch(err){
+                throw new Error("error occured while userList")
+            }
+        },
     },
     Mutation: {
-        registerUser: async (parent: unknown, args: {}, context: GraphQLContext) => {
-            try{
-                
-            }catch(err){
-
+        registerUser: async (parent: unknown, args: any, context: GraphQLContext) => {
+            try {
+                const body = args.input
+                const data = await context.prisma.user.create({
+                    data: body
+                })
+                return data
+            } catch (err) {
+                throw new Error("error occured while registerUser")
             }
         },
         // createProfile: async (_, { input }) => {
